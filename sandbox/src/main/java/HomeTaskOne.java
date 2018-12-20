285be2114fbacffeb911e8f353a379ba01523099
Chathura <crajapakse@itsupportme.by>
2018-12-20 14:23:57 +0300

    My first Home task

---
+++ sandbox/src/main/java/HomeTaskOne.java
@@ -0,0 +1,9 @@
+public class HomeTaskOne{
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello,");
+        System.out.println("Home Task No. 01: Prepare the infrastructure and create the first running program.");
+
+    }
+}
